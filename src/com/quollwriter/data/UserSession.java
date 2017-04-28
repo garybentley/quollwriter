08d1b9dab11b95bf8a1ecbefecdfb3c507204acf
Gary Bentley <gary@quollwriter.com>
2017-04-28 10:49:58 +1000

    Make sure that the session word count is returned when getWordCount is called from a UserSession.

--- src/com/quollwriter/data/UserSession.java
+++ src/com/quollwriter/data/UserSession.java
@@ -10,7 +10,7 @@ import com.quollwriter.ui.*;
  */
 public class UserSession extends Session
 {
-    
+
     public boolean sessionTargetReachedPopupShown = false;
     public boolean dailyTargetReachedPopupShown = false;
     private int lastSnapshotSessionWordCount = 0;
@@ -19,107 +19,107 @@ public class UserSession extends Session
     private Date dailyTargetReachedPopupShownDate = null;
     private Date weeklyTargetReachedPopupShownDate = null;
     private Date monthlyTargetReachedPopupShownDate = null;
-    
+
     public UserSession ()
     {
 
         this.init ();
-        
+
     }
-        
+
     public UserSession (Date start,
                         Date end,
                         int  wc)
     {
-        
+
         super (start,
                end,
                wc);
-        
+
         this.init ();
-        
+
     }
-    
+
     private void init ()
     {
-        
+
         String dv = UserProperties.get (Constants.TARGET_DAILY_TARGET_REACHED_POPUP_SHOWN_DATE);
-        
+
         Date currDate = Utils.zeroTimeFields (new Date ());
-        
+
         if (dv != null)
         {
-            
+
             try
             {
-            
+
                 this.dailyTargetReachedPopupShownDate = Utils.zeroTimeFields (new Date (Long.parseLong (dv)));
-                
+
                 this.dailyTargetReachedPopupShown = (currDate == this.dailyTargetReachedPopupShownDate);
-                
+
             } catch (Exception e) {
-                
+
                 Environment.logError ("Unable to parse daily target reached popup shown date",
                                       e);
-                
+
             }
-            
+
         }
-        
+
         dv = UserProperties.get (Constants.TARGET_WEEKLY_TARGET_REACHED_POPUP_SHOWN_DATE);
-        
+
         if (dv != null)
         {
-            
+
             try
             {
-            
+
                 this.weeklyTargetReachedPopupShownDate = new Date (Long.parseLong (dv));
-                
+
             } catch (Exception e) {
-                
+
                 Environment.logError ("Unable to parse weekly target reached popup shown date",
                                       e);
-                
+
             }
-            
+
         }
 
         dv = UserProperties.get (Constants.TARGET_MONTHLY_TARGET_REACHED_POPUP_SHOWN_DATE);
-        
+
         if (dv != null)
         {
-            
+
             try
             {
-            
+
                 this.monthlyTargetReachedPopupShownDate = new Date (Long.parseLong (dv));
-                
+
             } catch (Exception e) {
-                
+
                 Environment.logError ("Unable to parse monthly target reached popup shown date",
                                       e);
-                
+
             }
-            
+
         }
 
     }
-    
+
     public void shownDailyTargetReachedPopup ()
     {
-        
+
         Date d = Utils.zeroTimeFields (new Date ());
 
         this.dailyTargetReachedPopupShownDate = d;
         this.dailyTargetReachedPopupShown = true;
-        
+
         // Set a user property.
         UserProperties.set (Constants.TARGET_DAILY_TARGET_REACHED_POPUP_SHOWN_DATE,
-                            String.valueOf (d.getTime ()));                        
-        
+                            String.valueOf (d.getTime ()));
+
     }
-    
+
     public boolean shouldShowDailyTargetReachedPopup ()
     {
 
@@ -132,40 +132,40 @@ public class UserSession extends Session
             Date d = Utils.zeroTimeFields (new Date ());
 
             return d.after (this.dailyTargetReachedPopupShownDate);
-            
+
         }
 
         return true;
-        
+
     }
-    
+
     private Date getWeekDate ()
     {
-        
+
         GregorianCalendar gc = new GregorianCalendar ();
-        
+
         gc.set (Calendar.DAY_OF_WEEK,
                 gc.getFirstDayOfWeek ());
-                
+
         Date d = Utils.zeroTimeFields (gc.getTime ());
 
         return d;
-        
+
     }
-    
+
     public void shownWeeklyTargetReachedPopup ()
     {
-        
+
         Date d = this.getWeekDate ();
-        
+
         this.weeklyTargetReachedPopupShownDate = d;
-        
+
         // Set a user property.
         UserProperties.set (Constants.TARGET_WEEKLY_TARGET_REACHED_POPUP_SHOWN_DATE,
-                            String.valueOf (d.getTime ()));                        
-        
+                            String.valueOf (d.getTime ()));
+
     }
-    
+
     public boolean shouldShowWeeklyTargetReachedPopup ()
     {
 
@@ -175,40 +175,40 @@ public class UserSession extends Session
             Date d = this.getWeekDate ();
 
             return d.after (this.weeklyTargetReachedPopupShownDate);
-            
+
         }
 
         return true;
-        
+
     }
 
     private Date getMonthDate ()
     {
-        
+
         GregorianCalendar gc = new GregorianCalendar ();
-        
+
         gc.set (Calendar.DAY_OF_MONTH,
                 1);
-                
+
         Date d = Utils.zeroTimeFields (gc.getTime ());
 
         return d;
-        
+
     }
-    
+
     public void shownMonthlyTargetReachedPopup ()
     {
-        
+
         Date d = this.getMonthDate ();
-        
+
         this.monthlyTargetReachedPopupShownDate = d;
-        
+
         // Set a user property.
         UserProperties.set (Constants.TARGET_MONTHLY_TARGET_REACHED_POPUP_SHOWN_DATE,
-                            String.valueOf (d.getTime ()));                        
-        
+                            String.valueOf (d.getTime ()));
+
     }
-    
+
     public boolean shouldShowMonthlyTargetReachedPopup ()
     {
 
@@ -218,75 +218,83 @@ public class UserSession extends Session
             Date d = this.getMonthDate ();
 
             return d.after (this.monthlyTargetReachedPopupShownDate);
-            
+
         }
 
         return true;
-        
+
     }
 
     public void shownSessionTargetReachedPopup ()
     {
-        
+
         this.sessionTargetReachedPopupShown = true;
-        
+
     }
-    
+
     public boolean shouldShowSessionTargetReachedPopup ()
     {
-        
+
         return !this.sessionTargetReachedPopupShown;
-    
+
     }
-        
+
     public String toString ()
     {
-        
+
         return super.toString () + ", lastCurrent: " + this.currentSessionWordCount + ", current: " + this.getCurrentSessionWordCount ();
-        
+
     }
-    
+
+    @Override
+    public int getWordCount ()
+    {
+
+        return this.getCurrentSessionWordCount ();
+
+    }
+
     public void updateCurrentSessionWordCount (int wordCount)
     {
-        
+
         this.currentSessionWordCount += wordCount;
-        
+
     }
-    
+
     public int getCurrentSessionWordCount ()
     {
-        
+
         Map<ProjectInfo, AbstractProjectViewer> pvs = Environment.getOpenProjects ();
-        
+
         int c = this.currentSessionWordCount;
-        
+
         for (AbstractProjectViewer pv : pvs.values ())
         {
-            
+
             c += pv.getSessionWordCount ();
-            
+
         }
-        
-        return c;        
-        
+
+        return c;
+
     }
-    
+
     public UserSession createSnapshot ()
     {
-        
+
         int wc = this.getCurrentSessionWordCount () - this.lastSnapshotSessionWordCount;
-        
+
         Date end = new Date ();
-        
+
         UserSession s = new UserSession ((this.lastSnapshotSessionEnd != null ? this.lastSnapshotSessionEnd : this.getStart ()),
                                          end,
                                          wc);
-        
+
         this.lastSnapshotSessionEnd = end;
         this.lastSnapshotSessionWordCount = wc;
-        
+
         return s;
-        
+
     }
-        
+
 }
