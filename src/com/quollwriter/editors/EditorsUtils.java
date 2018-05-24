558cc4b01adf34e9e333819da8d9e55835edc9d8
Gary Bentley <gary@quollwriter.com>
2018-05-24 10:36:42 +1000

    Ensure that utf-8 is used for message encrypting/decrypting.

--- src/com/quollwriter/editors/EditorsUtils.java
+++ src/com/quollwriter/editors/EditorsUtils.java
@@ -37,89 +37,89 @@ import com.quollwriter.data.editors.*;
 
 public class EditorsUtils
 {
-                         
+
     public static BufferedImage getImageFromBase64EncodedString (String s)
                                                           throws Exception
     {
-        
+
         if (s == null)
         {
-            
+
             return null;
-                 
+
         }
-        
+
         // Decode.
-        byte[] bytes = com.quollwriter.Base64.decode (s);		
-		
+        byte[] bytes = com.quollwriter.Base64.decode (s);
+
 		return UIUtils.getImage (bytes);
-        
+
     }
-    
+
     public static String getImageAsBase64EncodedString (BufferedImage im)
                                                  throws Exception
     {
-        
+
         if (im == null)
         {
-            
+
             return null;
-            
+
         }
-		
+
 		byte[] bytes = UIUtils.getImageBytes (im);
-		        
+
         return com.quollwriter.Base64.encodeBytes (bytes);
-        
+
     }
 
-                 
+
 	public static PGPKeyPair generateKeyPair ()
 									   throws Exception
 	{
-		
+
         RSAKeyPairGenerator generator = new RSAKeyPairGenerator ();
-        
+
         generator.init (new RSAKeyGenerationParameters (new BigInteger ("10001", 16),//publicExponent
                                                         SecureRandom.getInstance("SHA1PRNG"),//prng
                                                         2048,//strength
                                                         100));
-                
+
         return new BcPGPKeyPair (PublicKeyAlgorithmTags.RSA_GENERAL,
                                  generator.generateKeyPair (),
                                  new java.util.Date ());
-				 
-	}				 
-	
+
+	}
+
 	public static byte[] getPGPPublicKeyByteEncoded (PGPPublicKey k)
 	{
-		
+
 		RSAPublicBCPGKey pubKey = (RSAPublicBCPGKey) k.getPublicKeyPacket ().getKey ();
-		
+
 		return pubKey.getEncoded ();
-		
+
 	}
-	
+
 	public static String getPGPPublicKeyBase64Encoded (PGPPublicKey k)
 	{
-		
+
         RSAPublicBCPGKey pubKey = (RSAPublicBCPGKey) k.getPublicKeyPacket ().getKey ();
 
 		return com.quollwriter.Base64.encodeBytes (pubKey.getEncoded ());
-		
+
 	}
-	
+
     public static PGPPublicKey convertToPGPPublicKey (byte[] bytes)
                                                throws Exception
     {
-        
+
         if (bytes == null)
         {
-            
+
             return null;
-            
+
         }
-        
+
         ByteArrayInputStream bin = new ByteArrayInputStream (bytes);
 
         RSAPublicBCPGKey pk = new RSAPublicBCPGKey (new BCPGInputStream (bin));
@@ -130,26 +130,26 @@ public class EditorsUtils
 
         return new PGPPublicKey (pkp,
                                  new BcKeyFingerprintCalculator ());
-        
-        
+
+
     }
-    
+
     public static String bytesToHex (byte[] bytes)
     {
-        
+
         StringBuilder sb = new StringBuilder();
-    
+
         for (byte b : bytes)
         {
-        
+
             sb.append(String.format("%02X", b));
-    
-        }        
-    
+
+        }
+
         return sb.toString ().toLowerCase ();
-        
+
     }
- 
+
     public static byte[] encrypt (String message,
                                   // Your private key, used for signature generation
                                   PGPPrivateKey privateKey,
@@ -161,17 +161,17 @@ public class EditorsUtils
         int BUFFER_SIZE = 8192; // should always be power of 2
 
         ByteArrayOutputStream bout = new ByteArrayOutputStream ();
-        
+
         ArmoredOutputStream outputStream = new ArmoredOutputStream(bout);
 
         // Init encrypted data generator
         BcPGPDataEncryptorBuilder encB = new BcPGPDataEncryptorBuilder (SymmetricKeyAlgorithmTags.AES_256);
         encB.setWithIntegrityPacket (true);
-        
+
         PGPEncryptedDataGenerator encryptedDataGenerator = new PGPEncryptedDataGenerator (encB);
-                
+
         encryptedDataGenerator.addMethod (new BcPublicKeyKeyEncryptionMethodGenerator (publicKey));
-        
+
         OutputStream encryptedOut = encryptedDataGenerator.open (outputStream,
                                                                  new byte[BUFFER_SIZE]);
 
@@ -181,16 +181,16 @@ public class EditorsUtils
 
         BcPGPContentSignerBuilder b = new BcPGPContentSignerBuilder (PublicKeyAlgorithmTags.RSA_GENERAL,
                                                                      HashAlgorithmTags.SHA512);
-        
+
         PGPSignatureGenerator signatureGenerator = new PGPSignatureGenerator (b);
-        
+
         signatureGenerator.init (PGPSignature.BINARY_DOCUMENT,
                                  privateKey);
-        
+
         PGPSignatureSubpacketGenerator spGen = new PGPSignatureSubpacketGenerator();
         spGen.setSignerUserID (false,
                                "user");
-        
+
         signatureGenerator.setHashedSubpackets (spGen.generate ());
 
         signatureGenerator.generateOnePassVersion (false).encode (compressedOut);
@@ -204,7 +204,7 @@ public class EditorsUtils
                                                              new Date (),
                                                              new byte[BUFFER_SIZE]);
 
-        byte[] messBytes = message.getBytes ();
+        byte[] messBytes = message.getBytes (java.nio.charset.StandardCharsets.UTF_8);
 
         literalOut.write (messBytes,
                           0,
@@ -215,26 +215,26 @@ public class EditorsUtils
 
         literalOut.close ();
         literalDataGenerator.close ();
-        
+
         signatureGenerator.generate ().encode (compressedOut);
-        
+
         compressedOut.close();
-        
+
         compressedDataGenerator.close();
-        
+
         encryptedOut.close();
-        
+
         encryptedDataGenerator.close();
 
-        outputStream.close();        
-        
+        outputStream.close();
+
         bout.flush ();
         bout.close ();
-        
+
         return bout.toByteArray ();
-        
+
     }
- 
+
 	public static byte[] decrypt (byte[]        message,
                                   // Your private key
                                   PGPPrivateKey privateKey,
@@ -242,11 +242,11 @@ public class EditorsUtils
                                   PGPPublicKey  publicKey)
                            throws Exception
     {
-    
+
         ByteArrayOutputStream bout = new ByteArrayOutputStream ();
-    
+
         InputStream in = new ByteArrayInputStream (message);
-        
+
         in = org.bouncycastle.openpgp.PGPUtil.getDecoderStream (in);
 
         PGPObjectFactory pgpF = new BcPGPObjectFactory (in);
@@ -271,145 +271,145 @@ public class EditorsUtils
 
         while (it.hasNext ())
         {
-            
+
             pbe = it.next();
-        
+
         }
 
         BcPublicKeyDataDecryptorFactory bkf = new BcPublicKeyDataDecryptorFactory (privateKey);
-        
+
         InputStream clear = pbe.getDataStream (bkf);
 
         PGPObjectFactory plainFact = new BcPGPObjectFactory(clear);
 
         Object messObj = null;
-        
+
         while (true)
         {
-            
+
             messObj = plainFact.nextObject ();
 
             if (messObj == null)
             {
-                
+
                 break;
-                
-            }            
-                        
+
+            }
+
             if (messObj instanceof  PGPCompressedData)
             {
 
                 PGPCompressedData cData = (PGPCompressedData) messObj;
                 PGPObjectFactory pgpFact = new BcPGPObjectFactory (cData.getDataStream ());
-    
+
                 PGPOnePassSignature osig = null;
                 PGPSignature sig = null;
-    
+
                 while (true)
                 {
-        
+
                     Object messObj2 = pgpFact.nextObject ();
 
                     if (messObj2 == null)
                     {
-                        
+
                         break;
-                        
+
                     }
-                        
+
                     if (messObj2 instanceof  PGPLiteralData)
                     {
-                    
+
                         PGPLiteralData ld = (PGPLiteralData) messObj2;
-            
+
                         InputStream unc = ld.getInputStream ();
                         int ch;
-            
+
                         while ((ch = unc.read()) >= 0)
                         {
-                        
+
                             if (osig != null)
                             {
-                                
+
                                 osig.update ((byte) ch);
-                                
+
                             }
-                        
+
                             bout.write(ch);
-                        
+
                         }
-                
+
                     }
-                    
+
                     if (messObj2 instanceof PGPOnePassSignatureList)
                     {
-                        
+
                         PGPOnePassSignatureList slist = (PGPOnePassSignatureList) messObj2;
-                        
+
                         for (int i = 0; i < slist.size (); i++)
                         {
-                            
+
                             osig = slist.get (i);
-                            
+
                             osig.init (new BcPGPContentVerifierBuilderProvider (),
                                        publicKey);
-                            
+
                         }
-                        
+
                     }
-                    
+
                     if (messObj2 instanceof PGPSignatureList)
                     {
-                        
+
                         PGPSignatureList slist = (PGPSignatureList) messObj2;
-                        
+
                         for (int i = 0; i < slist.size (); i++)
                         {
-                            
+
                             sig = slist.get (i);
 
                         }
-                        
+
                     }
 
                 }
-                
+
                 if ((osig != null)
                     &&
                     (sig != null)
                    )
                 {
-                    
+
                     if (!osig.verify (sig))
                     {
-                        
+
                         throw new GeneralException ("Unable to verify one pass signature");
-                        
+
                     }
-                    
+
                 }
-                
+
             }
-                    
+
         }
 
         if (pbe.isIntegrityProtected())
         {
-        
+
             if (!pbe.verify())
             {
-            
+
             	throw new GeneralException("Message failed integrity check");
-            
+
             }
-        
-        }        
-        
+
+        }
+
         bout.flush ();
         bout.close ();
-        
+
         return bout.toByteArray ();
-        
+
     }
-    
+
 }
