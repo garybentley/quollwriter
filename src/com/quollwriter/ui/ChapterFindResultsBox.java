11ba4a15ef2554d629d831dd4d0a71cbed77a09c
Gary Bentley <gary@quollwriter.com>
2018-09-12 13:50:24 +1000

    Update the white space?  Thanks git?

--- src/com/quollwriter/ui/ChapterFindResultsBox.java
+++ src/com/quollwriter/ui/ChapterFindResultsBox.java
@@ -14,44 +14,44 @@ import com.quollwriter.ui.renderers.*;
 import com.quollwriter.ui.panels.*;
 import com.quollwriter.ui.components.QTextEditor;
 
-public class ChapterFindResultsBox extends FindResultsBox
+public class ChapterFindResultsBox extends FindResultsBox<AbstractProjectViewer>
 {
 
     private Map<Chapter, List<Segment>> snippets = null;
     private Object highlightId = null;
     private QTextEditor highlightedEditor = null;
     private String forObjType = null;
-    
+
     public ChapterFindResultsBox (String                      title,
                                   String                      iconType,
                                   String                      forObjType,
                                   AbstractProjectViewer       viewer,
                                   Map<Chapter, List<Segment>> snippets)
     {
-                                            
+
         super (title,
                iconType,
                forObjType,
                viewer);
-                     
+
         this.forObjType = forObjType;
-                     
+
         this.snippets = snippets;
-                                            
+
     }
-    
+
     @Override
     public String getId ()
     {
-        
+
         return this.forObjType;
-        
+
     }
-    
+
     @Override
     public void initTree ()
     {
-        
+
         this.tree.setCellRenderer (new ChapterSnippetsTreeCellRenderer ());//new MultiLineTreeCellRenderer (this));
 
         DefaultMutableTreeNode tn = new DefaultMutableTreeNode (this.viewer.getProject ());
@@ -61,36 +61,36 @@ public class ChapterFindResultsBox extends FindResultsBox
 
         // Create the tree.
         ((DefaultTreeModel) this.tree.getModel ()).setRoot (tn);
-            
+
         int c = 0;
-        
+
         for (NamedObject n : this.snippets.keySet ())
         {
-            
+
             c += this.snippets.get (n).size ();
-            
+
         }
-        
+
         this.count = c;
-        
+
         this.updateItemCount (this.count);
-        
+
         this.setContentVisible (true);
-        
+
     }
-        
+
     @Override
     protected void handleViewObject (TreePath tp,
                                      Object   o)
     {
-               
+
         if (o instanceof Chapter)
         {
 
-            this.toggleTreePath (tp);        
-        
+            this.toggleTreePath (tp);
+
             DefaultMutableTreeNode node = (DefaultMutableTreeNode) tp.getLastPathComponent ();
-            
+
             // Damn son this is fugly.
             if ((node.getChildCount () == 1)
                 &&
@@ -98,23 +98,23 @@ public class ChapterFindResultsBox extends FindResultsBox
                                                                          node.getUserObject ())))
                )
             {
-                
+
                 DefaultMutableTreeNode n = node.getFirstLeaf ();
-                
+
                 TreePath tpp = UIUtils.getTreePathForUserObject ((DefaultMutableTreeNode) this.tree.getModel ().getRoot (),
                                                                  n.getUserObject ());
-                
+
                 this.handleViewObject (tpp,
-                                       n.getUserObject ());                
-                                
+                                       n.getUserObject ());
+
                 this.tree.setSelectionPath (tpp);
-                    
+
             }
-            
+
             return;
-            
+
         }
-                  
+
         if (o instanceof Segment)
         {
 
@@ -131,80 +131,80 @@ public class ChapterFindResultsBox extends FindResultsBox
                                      s);
 
         } else {
-            
+
             this.toggleTreePath (tp);
-                            
+
         }
-       
+
     }
-    
+
     public void clearHighlight ()
     {
-        
+
         if (this.highlightedEditor != null)
         {
-            
+
             this.highlightedEditor.removeHighlight (this.highlightId);
 
         }
-        
+
     }
-    
+
     public void showChapterSegment (final Chapter c,
                                     final Segment s)
     {
-        
+
         this.clearHighlight ();
-    
+
         final ChapterFindResultsBox _this = this;
-    
+
         this.viewer.viewObject (c,
                                 new ActionListener ()
         {
 
             public void actionPerformed (ActionEvent ev)
             {
-        
+
                 AbstractEditorPanel p = _this.viewer.getEditorForChapter (c);
-                                
+
                 try
                 {
-                    
+
                     p.scrollToPosition (s.getBeginIndex ());
-                    
+
                 } catch (Exception e) {
-                    
+
                     Environment.logError ("Unable to scroll to: " + s.getBeginIndex (),
                                           e);
-                    
+
                     return;
-                    
+
                 }
 
                 final QTextEditor ed = p.getEditor ();
-                
+
                 UIUtils.doLater (new ActionListener ()
-                {                           
-                
+                {
+
                     @Override
                     public void actionPerformed (ActionEvent ev)
                     {
-                
+
                         _this.highlightId = ed.addHighlight (s.getBeginIndex (),
                                                              s.getEndIndex (),
                                                              null,
                                                              true);
-                
+
                         _this.highlightedEditor = ed;
-                        
+
                     }
-                    
+
                 });
-                
+
             }
-            
+
         });
-        
-    }    
-                    
+
+    }
+
 }
